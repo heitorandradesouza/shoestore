ecd094105daa9d0412005532577c768af72edec4
Heitor Andrade <heitor.souza@unidas.com.br>
2022-09-18 17:16:58 -0300

    Product list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 
 const routes: Routes = [
   { path: 'list', component: ProductListComponent },
+  { path: 'detail', component: ProductDetailComponent },
 ]
 
 
--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -8,6 +8,104 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
 
   constructor() { }
+  productList: any = [
+    {
+      name: 'Nike 01',
+      price: '236',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Nike Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    },
+    {
+      name: 'Adidas 01',
+      price: '180',
+      primarayColor: '#000000',
+      secondaryColor: '#c58b25',
+      score: 5,
+      img: 'Green Shoe.png'
+    }
+  ]
 
   ngOnInit(): void {
   }
